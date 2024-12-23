9f9d76f9d17bf7b58643a98b78ce78b51877732e
kevin seligmann <kseligmann@uoc.edu>
2024-12-23 19:57:09 +0100

    Modelo de user

---
+++ controllers/UserController.js
@@ -0,0 +1,15 @@
+const { User } = require('../models/User')
+
+
+async function addUser(){
+    console.log("Add user")
+}
+
+async function login(){
+    console.log("Login")
+}
+
+module.exports = {
+    addUser,
+    login
+}
