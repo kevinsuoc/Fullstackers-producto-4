d46fd3b1b7c804fb4bfbe17736145a477b48c02d
kevin seligmann <kseligmann@uoc.edu>
2024-12-23 20:17:48 +0100

    Controlador de usuaroi

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,30 @@
 const { User } = require('../models/User')
 
 
-async function addUser(){
-    console.log("Add user")
+async function addUser(args){
+    const existingUser = await User.find({token: args.token}).exec()
+
+    if (existingUser)
+        return null;
+
+    const user = new User({
+        name: args.nombre,
+        password: args.description, 
+        token: args.token,
+    })
+    
+    await user.save()
+
+    return user;
 }
 
-async function login(){
-    console.log("Login")
+async function login(args){
+    const user = await User.find({name: args.name}).exec()
+
+    if (user.password != args.password)
+        return null
+
+    return user;
 }
 
 module.exports = {
